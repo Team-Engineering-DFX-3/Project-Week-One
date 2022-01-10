048a07170378cb5b11ca23de0600870c907738e3
Mitchell <dsilva.mitchell@gmail.com>
2022-01-10 10:14:19 +0000

    Fetched industry profiles from database along with images

--- IP_Frontend/starter/src/Component/Industry.jsx
+++ IP_Frontend/starter/src/Component/Industry.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import '../Component/css/App.css';
 import { Link } from "react-router-dom";
 import ContainerHeader from './Header/ContainerHeader';
+
 const Industry = () => {
     const [industries, setIndustries] = useState([]);
 
@@ -27,39 +28,35 @@ const Industry = () => {
     }, []);
 
     return (
-        <div className="row">
-            <div className="col-sm-10">
-                {
-                    industries.map(industry => {
-                        return (
-                            <div className="container shadow p-3 mb-5 bg-body rounded">
-                                <div class='row'>
-                                    <ul class='list col-sm body-align-left container shadow p-3 mb-5 bg-body rounded' id='left'>
-                                        <div className="card vacancy">
-                                            <div className="card-body">
-                                                <div className='row'>
-                                                    <Link to={`/industry/` + `${industry._id}`}>
-                                                        <ContainerHeader key={industry._id} title={industry.name} />
-                                                    </Link>
-                                                </div>
-
-                                                <h4 className="card-title mb-2 text-muted">{industry.description}</h4>
-                                                <h5 className="card-text">{industry.location}</h5>
-
-                                                {/* <a href="#" className="card-link">Card link</a>
-                                        <a href="#" className="card-link">Another link</a> */}
-                                            </div>
-                                        </div>
-                                    </ul>
+        <div className="body-align-center">
+            {
+                industries.map(industry => {
+                    return (
+                        <div className="container shadow mb-5 bg-body rounded ">
+                            <div className='row'>
+                                <Link to={`/industry/` + `${industry._id}`}>
+                                    <ContainerHeader key={industry._id} title={industry.name} />
+                                </Link>
+                            </div>
+                            <div className="container shadow mb-5 bg-body rounded ">
+                                <div className='row'>
+                                    <div className=" col-sm body-align-left" id='left'>
+                                        <ul className="list">
+                                            <li className="card-title mb-2 text-muted">{industry.description}</li>
+                                            <li className="card-text">{industry.location}</li>
+                                        </ul>
+                                    </div>
+                                    <div className='list col-md body-align-right' id='right'>
+                                        <img src={'http://127.0.0.1:4000/' + `${industry.image}`} alt="Industry Logo" className="img" />
+                                    </div>
                                 </div>
                             </div>
-                        )
-                    })
-                }
-            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
-
 };
 export default Industry;
 
