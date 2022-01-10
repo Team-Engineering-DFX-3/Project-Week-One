4136c76b8dcb9a5bcf681d5b7d917503a1763cf6
MatthewHWN <91743978+MatthewHWN@users.noreply.github.com>
2022-01-10 13:59:58 +0000

    small changes to get radhi's version working

--- IP_Frontend/starter/src/Component/Vacancy.jsx
+++ IP_Frontend/starter/src/Component/Vacancy.jsx
@@ -1,4 +1,4 @@
-import barclays from '../Component/images/barclays.png';
+
 import '../Component/css/App.css';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
@@ -6,18 +6,18 @@ import { Link, useLocation } from "react-router-dom";
 import ContainerHeader from './Header/ContainerHeader';
 import axios from 'axios';
 
-const Vacancy_Details = ({vacanciesData}) => {
-	const [vacancy, setVacancy] = useState({});
-	const { id } = useParams();
-	const location = useLocation();
+const Vacancy_Details = ({ vacanciesData }) => {
+    const [vacancy, setVacancy] = useState({});
+    const { id } = useParams();
+    const location = useLocation();
     const state = location.state;
-	/*const state = location.state|| {
+    /*const state = location.state|| {
         profile: {
             title: "Software Engineer", description: "Full Stack Developer", location: "London"
         }
     };*/
 
-	const getVacancy = async() => {
+    const getVacancy = async () => {
         try {
             const response = await axios.get('http://127.0.0.1:4000/vacancy/' + `${id}`);
             return response;
@@ -27,7 +27,7 @@ const Vacancy_Details = ({vacanciesData}) => {
         }
     };
 
-	useEffect(() => {
+    useEffect(() => {
         getVacancy().then((resp) => {
             if (resp !== "failure" && resp.status === 200) {
                 setVacancy(resp.data);
@@ -37,23 +37,23 @@ const Vacancy_Details = ({vacanciesData}) => {
         })
     }, []);
 
-	return (
+    return (
         <div className="container shadow p-3 mb-5 bg-body rounded">
             <div class='row'>
-               
+
                 <ul class='list col-sm body-align-left container shadow p-3 mb-5 bg-body rounded' id='left'>
                     <div className="card vacancy">
                         <div className="card-body">
                             <div className='row'>
                                 <ContainerHeader title="Card title" />
-                            </div> 
+                            </div>
                             <h5 className="card-title">Card title</h5>
                             <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
                             <p className="card-text">Some  text of the card.</p>
                             <Link to="/registerVacancy/">
-                            <button id="editButton" type="button" className="btn btn-primary">Register</button>
-                        	</Link>
-							
+                                <button id="editButton" type="button" className="btn btn-primary">Register</button>
+                            </Link>
+
                         </div>
                     </div>
                 </ul>
