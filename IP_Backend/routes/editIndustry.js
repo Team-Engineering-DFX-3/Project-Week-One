1e4ba60435ea88c0e2457421e43d6d9467c97ae6
Mitchell <dsilva.mitchell@gmail.com>
2022-01-10 18:48:33 +0000

    Modified the put request to handle edits without image updations

--- IP_Backend/routes/editIndustry.js
+++ IP_Backend/routes/editIndustry.js
@@ -5,14 +5,24 @@ const upload = multer({ dest: './public/data/uploads/' })
 import IndustryProfile from '../models/industryProfileSchema.js';
 
 router.route('/:id').put(upload.single('image'), async function (req, res) {
+    var imagePath, updatedData;
     const id = req.params.id;
-    const imagePath = req.file.path;
-    const image = imagePath.substring(7);
-
     const { name, description, location } = JSON.parse(req.body.industry);
+    const image = req?.file?.path;
+    if (image !== undefined) {
+        // const image = req.file.path;
+        imagePath = image.substring(7);
+        updatedData = { name: name, description: description, location: location, image: imagePath };
+    }
+    else {
+        updatedData = { name: name, description: description, location: location };
+    }
+
+    // const { name, description, location } = JSON.parse(req.body.industry);
     let updatedIndustry = await IndustryProfile.findOneAndUpdate(
-        { _id: id },
-        { name: name, description: description, location: location, image: image }
+        { _id: id }, updatedData,
+        // { name: name, description: description, location: location, image: imagePath },
+        { new: true }
     );
     res.status(200).send(updatedIndustry);
 });
@@ -25,5 +35,48 @@ router.route(`/:id`).get((req, res) => {
 });
 
 export { router as editIndustry };
+
+
+
+
+
+
+
+// import express from 'express';
+// const router = express.Router();
+// import multer from 'multer';
+// const upload = multer({ dest: './public/data/uploads/' })
+// import IndustryProfile from '../models/industryProfileSchema.js';
+
+// router.route('/:id').put(upload.single('image'), async function (req, res) {
+//     const id = req.params.id;
+//     var updatedData;
+//     var imageData;
+//     if (req.file.path !== '' || req.file.path !== undefined) {
+//         const { name, description, location } = JSON.parse(req.body.industry);
+//         const { image } = req.file.path;
+//         imageData = image.substring(7);
+//         console.log(imageData);
+//         updatedData = { name: name, description: description, location: location, image: imageData }
+//     }
+//     else {
+//         const { name, description, location } = JSON.parse(req.body.industry);
+//         updatedData = { name: name, description: description, location: location }
+//     }
+
+//     IndustryProfile.findOneAndUpdate({ _id: id }, updatedData, { new: true }).exec((error, industryprofiles) => {
+//         error ? res.status(400) : res.status(200).send(industryprofiles);
+//     });
+
+// });
+
+// router.route(`/:id`).get((req, res) => {
+//     const id = req.params.id;
+//     IndustryProfile.findOne({ _id: id }).exec((error, industryprofiles) => {
+//         error ? res.status(400) : res.status(200).send(industryprofiles);
+//     });
+// });
+
+// export { router as editIndustry };
 
 
