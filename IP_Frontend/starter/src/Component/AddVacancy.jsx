daca51661e9741c6be07a3b60b18fdfa766c4a79
Mitchell <dsilva.mitchell@gmail.com>
2022-01-11 22:12:32 +0000

    Added code to add vacancy and store in database

--- IP_Frontend/starter/src/Component/AddVacancy.jsx
+++ IP_Frontend/starter/src/Component/AddVacancy.jsx
@@ -1,21 +1,11 @@
 import '../Component/css/App.css';
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router';
-import { Link, useLocation } from "react-router-dom";
-// import image_holder from './images/image_holder.png';
+import { useState } from 'react';
+import { Link } from "react-router-dom";
 import ContainerHeader from './Header/ContainerHeader';
-// import FormData from 'form-data';
 import axios from 'axios';
 
 const AddVacancy = () => {
     const [vacancyData, setVacancyData] = useState({});
-    // const [industry, setIndustry] = useState({});
-    // const [file, setFile] = useState();
-    // const handleChangeFile = (e) => {
-    //     const file = e.target.files[0];
-    //     setFile(file);
-    // }
-
     const handleChange = e => {
         const { name, value } = e.target;
         setVacancyData({
@@ -44,49 +34,56 @@ const AddVacancy = () => {
 
     return (
         <>
-
-            <div className="container shadow mb-5 bg-body rounded">
-                <div className='row'>
-                    <ContainerHeader title="Add New Vacancy" />
+            <div className="body nospacing">
+                <div>
+                    <Link to={`/industries`}>
+                        <button id="editButton" type="button" className="btn btn-primary">View all Companies </button>
+                    </Link>
                 </div>
 
-                <form>
-                    <div className="container shadow mb-5 bg-body rounded" >
-                        <div className="row">
-                            <div className=" col-sm body-align-left" id='left'>
-                                <ul className="list">
-                                    <li><label htmlFor="company_name">Designation</label></li>
-                                    <li><input className='input' type="text" id="designation" name="designation" onChange={handleChange} placeholder="Designation to be offered" required /></li>
-                                    <li><label htmlFor="company_name">Company Name</label></li>
-                                    <li><input className='input' type="text" id="company_name" name="company_name" onChange={handleChange} placeholder="Company Name" required /></li>
-                                    <li><label htmlFor="company_description">Job Location</label></li>
-                                    <li><input className='input' type="text" id="job_location" name="job_location" onChange={handleChange} placeholder="Job Location" required /></li>
-                                    <li><label htmlFor="company_location">Mode of Work</label></li>
-                                    <li><input className='input' type="radio" id="company_location" name="mode" value="office" onChange={handleChange} /> In Office</li>
-                                    <li><input className='input' type="radio" id="company_location" name="mode" value="remote" onChange={handleChange} /> Remote</li>
-                                    <li><label htmlFor="required_qualification">Required Qualification</label></li>
-                                    <li><input className='input' type="text" id="qualification" name="qualification" onChange={handleChange} placeholder="Required qualification" required /></li>
-
-                                </ul>
-                            </div>
-                        </div>
-                    </div >
-                    <div className=' container shadow p-3 mb-5 bg-body rounded'>
-                        <div className="row">
-                            <div className="col-sm-5">
-                                <button type="submit" className="btn btn-primary btn-custom" onClick={handleSubmit}>Add Vacancy</button>
-                            </div>
-                            <div className="col-sm-5">
-                                <Link to="/">
-                                    <button type="button" className="btn btn-info btn-custom">Cancel Changes</button>
-                                </Link>
-                            </div>
-                            <div className="col-sm-2">
-                                <button type="reset" className="btn btn-danger btn-custom" form="industryProfileForm">Reset</button>
-                            </div>
-                        </div>
+                <div className="container shadow mb-5 bg-body rounded">
+                    <div className='row'>
+                        <ContainerHeader title="Add New Vacancy" />
                     </div>
-                </form >
+
+                    <form>
+                        <div className="container shadow mb-5 bg-body rounded" >
+                            <div className="row">
+                                <div className=" col-sm body-align-left" id='left'>
+                                    <ul className="list">
+                                        <li><label htmlFor="designation">Designation</label></li>
+                                        <li><input className='input' type="text" id="designation" name="designation" onChange={handleChange} placeholder="Designation to be offered" required /></li>
+                                        <li><label htmlFor="company_name">Company Name</label></li>
+                                        <li><input className='input' type="text" id="company_name" name="company_name" onChange={handleChange} placeholder="Company Name" required /></li>
+                                        <li><label htmlFor="job_location">Job Location</label></li>
+                                        <li><input className='input' type="text" id="job_location" name="job_location" onChange={handleChange} placeholder="Job Location" required /></li>
+                                        <li><label htmlFor="mode_of_work">Mode of Work</label></li>
+                                        <input type="radio" id="company_location" name="mode" value="Office" onChange={handleChange} /> In Office &nbsp;&nbsp;&nbsp;
+                                        <input type="radio" id="company_location" name="mode" value="Remote" onChange={handleChange} /> Remote <br />
+                                        <li><label htmlFor="required_qualification">Required Qualification</label></li>
+                                        <li><input className='input' type="text" id="qualification" name="qualification" onChange={handleChange} placeholder="Required qualification" required /></li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div >
+                        <div className=' container shadow p-3 mb-5 bg-body rounded'>
+                            <div className="row">
+                                <div className="col-sm-5">
+                                    <button type="submit" className="btn btn-primary btn-custom" onClick={handleSubmit}>Add Vacancy</button>
+                                </div>
+                                <div className="col-sm-5">
+                                    <Link to="/">
+                                        <button type="button" className="btn btn-info btn-custom">Cancel Changes</button>
+                                    </Link>
+                                </div>
+                                <div className="col-sm-2">
+                                    <button type="reset" className="btn btn-danger btn-custom" form="industryProfileForm">Reset</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form >
+                </div>
             </div>
         </>
     )
