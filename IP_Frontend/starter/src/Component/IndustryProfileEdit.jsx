14f8dc4e63148bc8579c23db96f0fcad60213d4a
Mitchell <dsilva.mitchell@gmail.com>
2022-01-11 22:14:37 +0000

    Added background image

--- IP_Frontend/starter/src/Component/IndustryProfileEdit.jsx
+++ IP_Frontend/starter/src/Component/IndustryProfileEdit.jsx
@@ -77,57 +77,58 @@ export default function IndustryProfileEdit() {
 
     return (
         <>
-            <div>
-                <Link to={`/industry/` + `${id}`}>
-                    <button id="editButton" type="button" class="btn btn-primary">
-                        Back to Company
-                    </button>
-                </Link>
-            </div>
-            <div className='container shadow mb-5 bg-body rounded'>
-                <div className='row'>
-                    <ContainerHeader title="Edit profile" />
+            <div className="body nospacing">
+                <div>
+                    <Link to={`/industry/` + `${id}`}>
+                        <button id="editButton" type="button" class="btn btn-primary">
+                            Back to Company
+                        </button>
+                    </Link>
                 </div>
+                <div className='container shadow mb-5 bg-body rounded'>
+                    <div className='row'>
+                        <ContainerHeader title="Edit profile" />
+                    </div>
 
-                <form >
-                    <div className='container shadow mb-5 bg-body rounded '>
-                        <div className="row">
-                            <div className=" col-sm body-align-left" id='left'>
-                                <ul className='list body-align-left' id='left' >
-                                    <li><label htmlFor="company_name">Company Name: </label><br /></li>
-                                    <li><input className='input' type="text" id="company_name" name="name" defaultValue={state.name} onChange={handleChange} placeholder="Company Name" required /><br></br></li>
-                                    <li><label htmlFor="description">Company Description: </label><br /></li>
-                                    <li><input className='input' type="text" id="description" name="description" defaultValue={state.description} onChange={handleChange} placeholder="Company Description" required /><br></br></li>
-                                    <li><label htmlFor="location">Company Location: </label><br /></li>
-                                    <li><input className='input' type="text" id="location" name="location" defaultValue={state.location} onChange={handleChange} placeholder="Company Location" required /><br /></li>
-                                </ul>
-                            </div>
+                    <form >
+                        <div className='container shadow mb-5 bg-body rounded '>
+                            <div className="row">
+                                <div className=" col-sm body-align-left" id='left'>
+                                    <ul className='list body-align-left' id='left' >
+                                        <li><label htmlFor="company_name">Company Name: </label><br /></li>
+                                        <li><input className='input' type="text" id="company_name" name="name" defaultValue={state.name} onChange={handleChange} placeholder="Company Name" required /><br></br></li>
+                                        <li><label htmlFor="description">Company Description: </label><br /></li>
+                                        <li><input className='input' type="text" id="description" name="description" defaultValue={state.description} onChange={handleChange} placeholder="Company Description" required /><br></br></li>
+                                        <li><label htmlFor="location">Company Location: </label><br /></li>
+                                        <li><input className='input' type="text" id="location" name="location" defaultValue={state.location} onChange={handleChange} placeholder="Company Location" required /><br /></li>
+                                    </ul>
+                                </div>
 
-                            <div className=' col-sm body-align-right' id='right'>
-                                <ul className='list body-align-right' id='right' >
-                                    <li><img src={state.image && `http://127.0.0.1:4000/` + `${state.image}`} alt="Industry Logo" className="img" /></li>
-                                    <li><input className="inputImage" type="file" name="image" onChange={handleChangeFile} accept="image/png, image/jpeg" defaultValue={state.image} required /></li>
-                                </ul>
+                                <div className=' col-sm body-align-right' id='right'>
+                                    <ul className='list body-align-right' id='right' >
+                                        <li><img src={state.image && `http://127.0.0.1:4000/` + `${state.image}`} alt="Industry Logo" className="img" /></li>
+                                        <li><input className="inputImage" type="file" name="image" onChange={handleChangeFile} accept="image/png, image/jpeg" defaultValue={state.image} required /></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className=' container shadow p-3 mb-5 bg-body rounded'>
-                        <div className="row">
-                            <div className="col-sm-5">
-                                <button type="submit" className="btn btn-primary btn-custom" onClick={handleSubmit}>Submit Profile Changes</button>
-                            </div>
-                            <div className="col-sm-5">
-                                <Link to={`/editIndustry/` + `${id}`}>
-                                    {/* <Link to="/"> */}
-                                    <button type="button" className="btn btn-info btn-custom">Cancel Changes</button>
-                                </Link>
-                            </div>
-                            <div className="col-sm-2">
-                                <button type="reset" className="btn btn-danger btn-custom" form="industryProfileForm">Reset</button>
+                        <div className=' container shadow p-3 mb-5 bg-body rounded'>
+                            <div className="row">
+                                <div className="col-sm-5">
+                                    <button type="submit" className="btn btn-primary btn-custom" onClick={handleSubmit}>Submit Profile Changes</button>
+                                </div>
+                                <div className="col-sm-5">
+                                    <Link to={`/editIndustry/` + `${id}`}>
+                                        <button type="button" className="btn btn-info btn-custom">Cancel Changes</button>
+                                    </Link>
+                                </div>
+                                <div className="col-sm-2">
+                                    <button type="reset" className="btn btn-danger btn-custom" form="industryProfileForm">Reset</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </form >
+                    </form >
+                </div>
             </div>
         </>
     )
