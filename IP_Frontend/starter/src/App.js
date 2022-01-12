c6db707830982bb8bb7b953d1b401eaba9ab1f06
Mitchell <dsilva.mitchell@gmail.com>
2022-01-12 21:28:42 +0000

    Added component routes for VacancyProfile and VacancyProfileEdit

--- IP_Frontend/starter/src/App.js
+++ IP_Frontend/starter/src/App.js
@@ -4,6 +4,8 @@ import Home from './Component/Home';
 import Header from './Component/Header/Header';
 import IndustryProfile from './Component/IndustryProfile';
 import IndustryProfileEdit from './Component/IndustryProfileEdit';
+import VacancyProfile from './Component/VacancyProfile';
+import VacancyProfileEdit from './Component/VacancyProfileEdit';
 import Vacancy from './Component/Vacancy';
 import VacancyRegister from './Component/VacancyRegister';
 import AddIndustry from './Component/AddIndustry';
@@ -22,10 +24,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/editIndustry" element={<IndustryProfileEdit />} />
         <Route path="/vacancy" element={<Vacancy />} />
+        <Route path="/editVacancy/:id" element={<VacancyProfileEdit />} />
         <Route path="/registerVacancy" element={<VacancyRegister />} />
         <Route path="/user" element={<UserProfile />} />
         <Route path="/addIndustry" element={<AddIndustry />} />
         <Route path="/addVacancy" element={<AddVacancy />} />
+        <Route path="/vacancyProfile/:id" element={<VacancyProfile />} />
         <Route path="/industries" element={<Industry />} />
         <Route path="/industry/:id" element={<IndustryProfile />} />
         <Route path="/editIndustry/:id" element={<IndustryProfileEdit />} />
