a89091e3015ba457240f0f65f044f88c49f37f23
Mitchell <dsilva.mitchell@gmail.com>
2022-01-13 13:01:58 +0000

    Changed styles and added id to path

--- IP_Frontend/starter/src/Component/Vacancies.jsx
+++ IP_Frontend/starter/src/Component/Vacancies.jsx
@@ -31,47 +31,45 @@ const Vacancies = (props) => {
 
     return (
         <>
-            {
-                (vacancyData.length > 0) ? <h1> Vacancies</h1> : <h1> Sorry!! No Vacancies Currently.</h1>
-            }
-            <div>
-                <div className="body-align-center body">
-
+            <div className="body-align-center body">
+                <div className="container body-align-center shadow p-3 mb-5 bg-body rounded">
                     {
-                        vacancyData.map(vacancy => {
-                            return (
-
-                                <div className="container shadow mb-5 bg-body rounded ">
-                                    <div className='row'>
-                                        <Link to={`/vacancyProfile/` + `${vacancy._id}`}>
-                                            <ContainerHeader key={vacancy._id} title={vacancy.designation} />
-                                        </Link>
-                                    </div>
-                                    <div className="container shadow mb-5 bg-body rounded ">
-                                        <div className='row'>
-                                            <div className=" col-sm body-align-left" id='left'>
-                                                <ul className="list">
-                                                    <li className="card-text"><b>Company: </b>{vacancy.company_name}</li>
-                                                    <li className="card-text"><b>Job Location: </b>{vacancy.job_location}</li>
-                                                    <li className="card-text"><b>Mode of Work: </b>{vacancy.mode}</li>
-                                                    <li className="card-text"><b>Qualification Required: </b>{vacancy.qualification}</li>
-                                                </ul>
-                                                <Link to="/registerVacancy/">
-                                                    <button id="editButton" type="button" className="btn btn-primary">Register</button>
-                                                </Link>
+                        (vacancyData.length > 0) ? <h1> Vacancies</h1> : <h1> Sorry!! No Vacancies Currently.</h1>
+                    }
+                    <div className='row'>
+                        {
+                            vacancyData.map(vacancy => {
+                                return (
+                                    <div className="container_small card-body">
+                                        <div className="row">
+                                            <Link to={`/vacancyProfile/` + `${vacancy._id}`}>
+                                                <ContainerHeader key={vacancy._id} title={vacancy.designation} />
+                                            </Link>
+                                        </div>
+                                        <div className="container shadow mb-5 bg-body rounded ">
+                                            <div className='row'>
+                                                <div className=" col-sm body-align-left" id='left'>
+                                                    <ul className="list">
+                                                        <li className="card-text"><b>Company: </b>{vacancy.company_name}</li>
+                                                        <li className="card-text"><b>Job Location: </b>{vacancy.job_location}</li>
+                                                        <li className="card-text"><b>Mode of Work: </b>{vacancy.mode}</li>
+                                                        <li className="card-text"><b>Qualification Required: </b>{vacancy.qualification}</li>
+                                                    </ul>
+                                                    <Link to={`/registerVacancy/` + `${vacancy._id}`}>
+                                                        <button id="editButton" type="button" className="btn btn-primary">Register</button>
+                                                    </Link>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-
-                            )
-                        })
-                    }
+                                )
+                            })
+                        }
+                    </div>
                 </div>
             </div>
         </>
     )
-
 }
 
 
