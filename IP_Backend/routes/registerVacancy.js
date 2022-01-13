9c6f40d3f69f0644e62d3d43028eb55863c899a0
Mitchell <dsilva.mitchell@gmail.com>
2022-01-13 12:54:08 +0000

    Added code for adding registration data to database

--- IP_Backend/routes/registerVacancy.js
+++ IP_Backend/routes/registerVacancy.js
@@ -3,13 +3,16 @@ const router = express.Router();
 import VacancyRegisterData from '../models/vacancyRegisterSchema.js';
 
 router.route('/').post((req, res) => {
-    const vacancy = new VacancyRegisterData(req.body);
-    vacancy.save(err => {
+    var registerData = {};
+    const { applicant_name, apply_discipline, company_name, designation, applicant_experience, applicant_location } = req.body;
+    registerData = { applicant_name: applicant_name, apply_discipline: apply_discipline, company_name: company_name, designation: designation, applicant_experience: applicant_experience, applicant_location: applicant_location };
+    const vacancyRegister = new VacancyRegisterData(registerData);
+    vacancyRegister.save(err => {
         if (err) {
             res.send(err);
         }
         else {
-            res.send({ message: `Registering Job Vacancy`, registration: req.body });
+            res.send({ message: `Registered Successfully for Vacancy.`, registration: registerData });
         }
     });
 });
